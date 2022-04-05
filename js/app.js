393a86e819c9bdd8f6b7cb415c198f105f518031
John Dietrich <john@thoughtlesslabs.com>
2022-04-05 15:18:41 -0400

    Basic search functionality working

---
+++ js/app.js
@@ -0,0 +1,37 @@
+// Search for a tv show using some form of category or search term
+// Retrieve results from api
+// Display results on page
+// Results should include: Title, date, if it's running still, and an image
+
+const form = document.querySelector("#search-form");
+const resultsList = document.querySelector("#results");
+
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const userSearch = form.elements.query.value;
+    const results = await searchTvShows(userSearch);
+    await addSearchResults(results);
+})
+
+const searchTvShows = async (search) => {
+    const config = { params: { q: search } };
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config );
+    return res.data
+}
+
+const addSearchResults = async (display) => {
+    for ( let shows of display) {
+        const newIMG = document.createElement("IMG");
+        const newLI = document.createElement("li");
+        if ( shows.show.image ) {
+            const showIMG = shows.show.image.medium;
+            newIMG.src = showIMG
+        }
+
+        const showName = shows.show.name;
+        newLI.innerHTML = showName;
+
+        resultsList.append(newLI);
+        resultsList.append(newIMG);
+    }
+}
